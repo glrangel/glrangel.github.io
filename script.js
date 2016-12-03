71bb0be3443ff039c43ef82fa7f2c37ba8a59752
Gabriel Rangel <glrangel25@gmail.com>
2016-12-03 14:29:41 -0800

    Aesthetic changes to background image

--- script.js
+++ script.js
@@ -2,16 +2,32 @@ $(document).ready(function(){
 	if (jQuery) {  
     	console.log("jQuery Exists");
     }
+    /* auto scroll to menu item location */
 	$('a[href^="#"]').on('click', function(event) {
 	    var target = $(this.getAttribute('href'));
 	    console.log(target);
 	    if( target.length ) {
-	    		    console.log("in if statment.\n");
-
 	        event.preventDefault();
 	        $('html, body').stop().animate({
 	            scrollTop: target.offset().top -50
 	        }, 1000);
 	    }
 	});
+
+
+
 });
+// $(window).onload = function()
+// {
+// }
+function fade(){
+	console.log("woop");
+			$("#lol").fadeOut(1000);
+
+	$("#title").addClass("load");
+	$("#subtitle").addClass("load");
+	$(".navbar").addClass("load");
+
+
+	console.log($("#title").hasClass("load"));
+}
