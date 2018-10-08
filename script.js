a1b745c12b3838fdc9c55765bb0db9c9c3e100a6
Gabriel Rangel <GRangel@blueskynetwork.com>
2018-10-08 14:30:23 -0700

    Fix scroll to top

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 function scrollToID(tag){
     console.log(tag);
-    //close mobile  menu if open
-    if($(".menu").is(':visible')){
-        $( ".menu" ).slideToggle( "slow", function() {
-            $( ".cross" ).hide();
-            $( ".hamburger" ).show();
-        });
-    }
-
     var dest = $(tag).offset().top-37;
-    $('html').animate({scrollTop: dest},1000);
+    $('body').animate({scrollTop: dest},1000);
 
 }
